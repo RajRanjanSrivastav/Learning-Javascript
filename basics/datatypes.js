50be31b6d5d41eaf95d8d5738a327cc8dd10ebe1
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-08-02 14:56:53 +0530

    learn more

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -4,3 +4,12 @@ city = "lucknow"
 
 // name="lala"    // value of const can't be changed after declaration
 console.table([counter, name, city]);
+
+
+// data conversion
+
+let cnt = "33bh";
+console.log(typeof(cnt))
+let change = Number(cnt);
+console.log(typeof(change));
+console.log(change)
