b5fe27af2d981f9066977d98d27fd503c5bbc42d
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-08-02 16:02:37 +0530

    learn more

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -13,3 +13,17 @@ console.log(typeof(cnt))
 let change = Number(cnt);
 console.log(typeof(change));
 console.log(change)
+
+console.log(2**4)
+
+// postfix increment
+let a = 5
+let b = a++;
+console.log(b);
+console.log(a);
+
+//prefix increment
+a=5
+b=++a
+console.log(b);
+console.log(a);
