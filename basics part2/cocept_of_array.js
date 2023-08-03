9eaed49cbfc4b0b1613e7ae368bc935375fb7c06
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-08-03 15:40:13 +0530

    learn more

--- basics part2/cocept_of_array.js
+++ basics part2/cocept_of_array.js
@@ -3,6 +3,15 @@ console.log("a: ",arr)
 const newarr = new Array(1,2,3,'raj bhai')
 // console.log(newarr);
 
+//difference btween const arr = new Array() and const arr = []
+//the main difference is you can set the size of array in new Array() syntax
+//lets take a example
+
+const myarr1 = [4]
+const myarr2 = new Array(4);
+console.log(myarr1===myarr2) //this gives false
+console.log(myarr1.length,"and",myarr2.length) //myarr1 gives 1 and myarr2 gives 4
+
 
 //difference between in slice and splice
 // console.log(arr.slice(1,4))
