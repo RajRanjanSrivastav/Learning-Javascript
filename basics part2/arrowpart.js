337f29029747ef33fe4d068fb7c512d5e5c51a97
RAJ RANJAN SRIVASTAV <srivastavrajranjan75@gmail.com>
2023-08-20 11:56:06 +0530

    learn about iief

--- basics part2/arrowpart.js
+++ basics part2/arrowpart.js
@@ -1,19 +1,40 @@
 // console.log(this)
 
-const user = {
-    name:"raj ranjan srivastav",
-    id:34,
-    welcome: function(){
-        console.log(`Good afternoon Mr.${this.name}`)
-        console.log(this)
-    }
-}
-user.welcome()
-user.name="shiva"
-user.welcome()
-console.log(this)
+// const user = {
+//     name:"raj ranjan srivastav",
+//     id:34,
+//     welcome: function(){
+//         console.log(`Good afternoon Mr.${this.name}`)
+//         console.log(this)
+//     }
+// }
+// user.welcome()
+// user.name="shiva"
+// user.welcome()
+// console.log(this)
 
-function call(){
-    console.log(this)
-}
-call()
+// function call(){
+//     console.log(this)
+// }
+// call()
+
+
+// const myfun = (a,b) => {
+//     return a+b;
+// }
+
+// const myfun = (a,b) => a+b
+
+// const myfun = (a,b) => (a+b)
+
+// const myfun =(a,b) =>({username:"raj ranjan srivastav"})
+
+// console.log(myfun(5,8))
+
+//IIEF
+
+(function chai(){console.log("connected")})();
+
+// in this fucntion if we not include semicolon; then it throw error because compiler excute the fuction but not know where to close so must include semicolon;
+
+(() => {console.log("connected")})()
